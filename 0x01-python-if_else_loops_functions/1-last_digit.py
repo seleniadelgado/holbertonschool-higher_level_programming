e96cebfa9014e2792ef9627ea5e8d9ff66636ccd
--get <angiedelgado08@gmail.com>
2018-12-05 00:26:46 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number >= 0:
         print("{} and is greater than five".format(lastnum))
     elif lastnum == 0:
         print("{} and is 0".format(lastnum))
-    elif lastnum < 6 and number != 0:
+    elif lastnum < 6 and lastnum != 0:
         print("{} and is less than 6 and not 0".format(lastnum))
 elif number < 0:
     negnum = (((number * -1) % 10)*-1)
