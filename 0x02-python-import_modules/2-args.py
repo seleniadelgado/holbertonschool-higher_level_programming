49e87049a9b1a8cbd41b5aea4369eae0edb4748b
--get <angiedelgado08@gmail.com>
2018-12-06 03:20:33 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)-1
+    if args == 1:
+        print("{:d} argument:".format(args))
+        print("{:d}: {}".format(args, sys.argv[1]))
+    if args == 0:
+        print("{:d} arguments.".format(args))
+    if args >= 2:
+        print("{:d} arguments:".format(args))
+        for i in range(1,(len(sys.argv))):
+            print("{:d}: {}".format(i, sys.argv[i]))
