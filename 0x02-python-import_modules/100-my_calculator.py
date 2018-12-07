cd58a0e2fd9754042f4fa70684404b5f1c47a334
--get <angiedelgado08@gmail.com>
2018-12-07 00:54:14 +0000

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import calculator_1.py
+    if args != 3
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif operator = "+":
+        print("{:d}{}{:d}={}".format(a, operator, b, add(a, b))
+    elif operator="-":
+        print("{:d}{}{:d}={}".format(a, operator, b, sub(a, b))
+    elif operator = "*":
+        print("{:d}{}{:d}={}".format(a, operator, b, mul(a, b))
+    elif operator = "/":
+        print("{:d}{}{:d}={}".format(a, operator, b, div(a, b))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
