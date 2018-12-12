f046b6bdbc708c4eae0ed34670d08aa166c03a8a
--get <angiedelgado08@gmail.com>
2018-12-12 05:39:46 +0000

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        a = sentence(None,)
+        return (0, None)
+    elif sentence:
+        a = len(sentence)
+        b = sentence[0]
+    return (a, b,)
