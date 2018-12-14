0dba35670f39d821129ce7022365516336eb4caa
--get <angiedelgado08@gmail.com>
2018-12-14 06:10:25 +0000

    11-mutiply_list_map.py

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    return map(lambda(mutiply_list_map(newlist=my_list.values * 2)))
+    return list(map(lambda x: x * number, my_list))
