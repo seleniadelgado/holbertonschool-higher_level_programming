010ad8cf393ee7dc64672a43a144e882180892be
--get <angiedelgado08@gmail.com>
2019-01-03 02:22:36 +0000

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("".format())
+    else:
+        print("Inside result:".format())
+    finally:
+        print("{}".format(result))
+    return result
