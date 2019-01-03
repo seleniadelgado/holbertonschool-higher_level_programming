cc2a37a6d98836610437d4858aa74960943c7a82
--get <angiedelgado08@gmail.com>
2019-01-03 02:33:15 +0000

    fixed print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,9 +4,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("".format())
-    else:
-        print("Inside result:".format())
+        result = None
+        return None
     finally:
+        print("Inside result: ".format(result), end="")
         print("{}".format(result))
     return result
