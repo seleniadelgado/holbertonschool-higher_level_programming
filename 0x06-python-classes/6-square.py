1259f72703a5f6d67ef072a186c0d669edd0d26a
--get <angiedelgado08@gmail.com>
2019-01-04 23:27:33 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square():
     """class"""
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
