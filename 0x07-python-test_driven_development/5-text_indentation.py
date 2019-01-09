d0c003f1518d00ae5666b46bf9dcae13cc8650df
--get <angiedelgado08@gmail.com>
2019-01-09 00:55:30 +0000

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < (len(text)):
+        print(text[i], end="")
+        if text[i] == "?" or text[i] == "." or text[i] == ":":
+            print("\n")
+            while i < len(text) - 1 and text[i + 1] == " ":
+                i += 1
+        i += 1
