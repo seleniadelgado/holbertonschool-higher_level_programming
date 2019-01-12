c8db5aeeed33b44d8f5eea9ed1a0e62a19ef96a5
--get <angiedelgado08@gmail.com>
2019-01-12 02:50:46 +0000

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,18 +2,15 @@
 def matrix_divided(matrix, div):
     new_matrix = []
     if type(matrix) is not list:
-        raise TypeError("matrix must be a \
-        matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         for box in matrix:
             if type(box) is not list:
-                raise TypeError("matrix must be a \
-                matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
        if len(matrix) > 1:
         row = len(matrix[0])
         for rows in matrix:
             if len(rows) is not row:
-                raise TypeError(
-                    "each row of the matrix must have the same size")
+                raise TypeError("each row of the matrix must have the same size")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -22,8 +19,7 @@ def matrix_divided(matrix, div):
         littlelist = []
         for b in a:
             if type(b) is not int and type(b) is not float:
-                raise TypeError('matrix must be a \
-                matrix (list of lists) of integers/floats')
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
             result = b / div
             result = round(result, 2)
             littlelist.append(result)
