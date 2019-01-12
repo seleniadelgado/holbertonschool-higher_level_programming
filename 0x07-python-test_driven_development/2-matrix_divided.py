8d31b826c180bc17827741cb60ee6d7bb9537091
--get <angiedelgado08@gmail.com>
2019-01-12 03:15:59 +0000

    changed var to c and d

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     new_matrix = []
-    a = "matrix must be a matrix (list of lists) of integers/floats"
-    b = "each row of the matrix must have the same size"
+    c = "matrix must be a matrix (list of lists) of integers/floats"
+    d = "each row of the matrix must have the same size"
     if type(matrix) is not list:
-        raise TypeError(a)
+        raise TypeError(c)
         for box in matrix:
             if type(box) is not list:
-                raise TypeError(a)
+                raise TypeError(c)
     if len(matrix) > 1:
         row = len(matrix[0])
         for rows in matrix:
             if len(rows) is not row:
-                raise TypeError(b)
+                raise TypeError(d)
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -21,7 +21,7 @@ def matrix_divided(matrix, div):
         littlelist = []
         for b in a:
             if type(b) is not int and type(b) is not float:
-                raise TypeError(a)
+                raise TypeError(c)
             result = b / div
             result = round(result, 2)
             littlelist.append(result)
