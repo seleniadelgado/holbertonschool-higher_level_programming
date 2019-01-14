3c3ab4e0dbcf028363e1bb7f778590b3f519c2a6
--get <angiedelgado08@gmail.com>
2019-01-14 01:34:14 +0000

    changes to unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -25,15 +25,15 @@ class TestMaxInteger(unittest.TestCase):
 
     def text_max_integer(self):
         """test for if "one negative number in the list" exists"""
-        self.assertEqual(max_integer([-1, 0, 0]), -1)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
     def test_max_integer(self):
         """test for only negative numbers in the list"""
-        self.assertEqual(max_integer([-1, -4]), -1)
+        self.assertEqual(max_integer([-1, -4, -9]), -1)
 
     def test_max_integer(self):
         """test for only one element in list"""
-        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([5]), 5)
 
 if __name__ == '__main__':
     unittest.main()
