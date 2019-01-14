7f07a9d6fbff2ad9a6889c8eea171ec58de3906b
--get <angiedelgado08@gmail.com>
2019-01-14 02:05:53 +0000

    changed names of tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,31 +7,31 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_max_integer(self):
+    def test_empty_list(self):
         """ test for an empty list"""
         self.assertEqual(max_integer([]), None)
 
-    def test_max_integer(self):
+    def test_biggest_number_end(self):
         """test if biggest number is at the end"""
         self.assertEqual(max_integer([0, float("inf")]), float("inf"))
 
-    def test_max_integer(self):
+    def test_biggest_number_beginning(self):
         """test if biggest number is at the beginning"""
         self.assertEqual(max_integer([float("inf"), 0]), float("inf"))
 
-    def test_max_integer(self):
+    def test_biggest_number_middle(self):
         """test if biggest number is in the middle"""
         self.assertEqual(max_integer([0, float("inf"), 0]), float("inf"))
 
-    def test_max_integer(self):
+    def test_biggest_negative_number(self):
         """test for if "one negative number in the list" exists"""
         self.assertEqual(max_integer([2, -1]), 2)
 
-    def test_max_integer(self):
+    def test_all_negative_numbers(self):
         """test for only negative numbers in the list"""
         self.assertEqual(max_integer([-2, -4, -9]), -2)
 
-    def test_max_integer(self):
+    def test_only_one_number(self):
         """test for only one element in list"""
         self.assertEqual(max_integer([5]), 5)
 
