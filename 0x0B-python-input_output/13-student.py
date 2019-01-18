c1b203715a27033f2839e3b26a01fb1003e71098
--get <angiedelgado08@gmail.com>
2019-01-18 06:12:29 +0000

    pep8 errors

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
+
 def __init__(self, first_name, last_name, age):
         """defines attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
- def to_json(self, attrs=None):
+
+def to_json(self, attrs=None):
         if type(attrs) is list:
             dict = {}
             for i in attrs:
@@ -14,5 +17,6 @@ def __init__(self, first_name, last_name, age):
             return dict
         else:
             return self.__dict__
+
 
 def reload_from_json(self, json):
