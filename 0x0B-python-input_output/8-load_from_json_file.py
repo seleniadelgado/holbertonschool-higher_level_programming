1920567ed3621671ed32aac381fe571bafbcb641
--get <angiedelgado08@gmail.com>
2019-01-18 04:26:04 +0000

    8-load_from_json_file.py

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""function that creates an Object from "JSON file"""
+
+
+def load_from_json_file(filename):
+    with open("filename") as f:
+        return json.load(f)
