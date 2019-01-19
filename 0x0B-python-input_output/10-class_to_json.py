a54621d62e0b5ceb2760a507d579ca62eb09bed1
--get <angiedelgado08@gmail.com>
2019-01-19 01:20:53 +0000

    fixing errors

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,8 +2,8 @@
 """a function that returns the dictionary description with simple data
 structure (list, dictionary, string, integer and boolean) for JSON
 serialization of an object"""
-import json
 
 
 def class_to_json(obj):
+    """defines module."""
     return obj.__dict__
