2e89d10227ab3997c5958e346407fb151868afdb
--get <angiedelgado08@gmail.com>
2019-01-20 23:04:52 +0000

    9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-import json
 import sys
 """that adds all arguments to a Python list, and then save them to a file"""
 
-for x in sys.argv:
-listit = []
-list.append()
-save_to_json_file(my_obj, filename) #writes object to a text file
-load_from_json_file(filename) #creates an object from a Json file
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+try:
+     my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+args = sys.argv[1:]
+
+for i in args:
+    my_list.append(i)
+save_to_json_file(my_list, "add_item.json") #writes object to a text file
