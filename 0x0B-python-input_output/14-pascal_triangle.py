6648cf2cc2992b757e7f6e241177633cabf5fc03
--get <angiedelgado08@gmail.com>
2019-01-21 03:44:46 +0000

    14-pascal_triangle.py

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""pascal triangle"""
+"""create a function that returns a rep of pascal triangle of n"""
 
 
 def pascal_triangle(n):
     dlist = []
-    if n <= 0
-    return dlist
-    else:
+    uno = 1
+    if n <= 0:
         return dlist
+    for row in range(n):
+        items = []
+        for i in range(0, row + 1):
+            if i == 0 or i == row or row == 0:
+                items.append(uno)
+            else:
+                items.append(dlist[row - 1][i] + dlist[row - 1][i - 1])
+        dlist.append(items)
+    return dlist
