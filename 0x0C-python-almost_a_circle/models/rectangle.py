8cb362813045a6f401958f56199f2c403897af3d
--get <angiedelgado08@gmail.com>
2019-01-23 03:16:00 +0000

    python almost a circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Rectangle(Base):
+
+
+    def __init__(self, width, height, x=0, y=0, id=None)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    
