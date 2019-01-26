f2d64ae8af700537d9732c728d05da94c162131e
--get <angiedelgado08@gmail.com>
2019-01-26 01:37:23 +0000

    all tasks up to 19 and test files

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for base.py([..])
+"""
+
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+
+
+    def test_empty(self)
+        """test for an empty string"""
