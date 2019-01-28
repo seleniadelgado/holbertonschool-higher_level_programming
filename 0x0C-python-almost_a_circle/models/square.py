42ec941959dcb72eae63cf526d78983f87d7f97e
--get <angiedelgado08@gmail.com>
2019-01-28 06:03:44 +0000

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
 """importing from rectangle, class that inherits from rectangle"""
+from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """new class Square inherits from rectangle"""
