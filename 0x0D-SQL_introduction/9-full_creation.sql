a9f7177e48142a179ba3ea3e251a4f4bc16632ce
--get <angiedelgado08@gmail.com>
2019-03-05 03:15:45 +0000

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,4 @@
 DROP TABLE IF EXISTS `second_table`;
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (id, name, score)
-<<<<<<< HEAD
 VALUES(1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
-=======
-VALUES('1', 'John', '10'),('2', 'Alex', '3'),('3', 'Bob', '14'),('4', 'George', '8');
->>>>>>> cf6972816ce1c158a429d884b5b006f2a73e3847
