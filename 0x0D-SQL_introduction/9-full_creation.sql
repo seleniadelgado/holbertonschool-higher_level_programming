38e9cde5bd27b9ab5eaad5dcce7e099abfb730bb
--get <angiedelgado08@gmail.com>
2019-03-05 03:12:29 +0000

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create table with rows
 DROP TABLE IF EXISTS `second_table`;
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table` (`id, name, score)
-VALUES('1', 'John', '10'),('2', 'Alex', '3'),('3', 'Bob', '14'),('4', 'George', '8');
+INSERT INTO `second_table` (id, name, score)
+VALUES(1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
