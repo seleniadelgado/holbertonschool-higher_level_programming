f6a3f970a2c2a9dad05216b7a5de205c7804d57d
seleniadelgado <angiedelgado08@gmail.com>
2019-03-06 22:28:26 +0000

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genres and display number of Shows linked to each other
 SELECT genre.name, 
-COUNT genre.id AS number_of_shows
+COUNT(genre.id) AS number_of_shows
 FROM tv_show_genres 
 INNER JOIN genres
 ON genre_id = genres.id
