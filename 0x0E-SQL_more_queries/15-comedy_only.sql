91e86cda1cb996b9fb0de968c40e6e710ca0c38c
--get <angiedelgado08@gmail.com>
2019-03-07 01:52:31 +0000

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,9 @@
--- list all comedy shows. 
+-- list all comedy shows.
+SELECT title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE name = "Comedy"
+ORDER by title ASC;
