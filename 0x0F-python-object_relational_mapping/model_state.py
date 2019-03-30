bf1fc9baf528e8cfa015f92d9183949f23584824
--get <angiedelgado08@gmail.com>
2019-03-30 04:41:27 +0000

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sqlalchemy import Table, Column, String, Integer
+from sqlalchemy.ext.declarative_base import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class that inherits from base"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
