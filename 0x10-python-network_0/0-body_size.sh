3883e9a70671b048c2d1c195452dcdddd46d54d8
--get <angiedelgado08@gmail.com>
2019-04-11 01:40:51 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes URL, sends a request to that URL, and displays the size
 # of the body
-curl -w "%{size_download}\n" $1 -s -o test
+curl -w "%{size_download}\n" "$1" -s -o /dev/null
