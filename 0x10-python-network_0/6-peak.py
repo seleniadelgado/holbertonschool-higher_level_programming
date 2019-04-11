9d3bf2a2cc0e012e5965d5ffe328fb00b69e992b
--get <angiedelgado08@gmail.com>
2019-04-11 05:28:27 +0000

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""find the peak"""
+def find_peak(list_of_integers):
+    if not isinstance(list_of_integers, list)
+        return None
+    if not list_of_integers:
+        return None
+    for first in list_of_integers:
+        
