e43d5ee8c376b043df4e6888ba78a36a8be68903
--get <angiedelgado08@gmail.com>
2019-04-16 04:18:06 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import requests
-from sys import argv
+import sys
 """Take in a URL, send a request, and display the val of X-Request-Id"""
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    r = requests.get(sys.argv[1])
     r = r.headers['X-Request-Id']
-    print("{}".format(r))
+    print(r)
