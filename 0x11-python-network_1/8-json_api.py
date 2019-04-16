d62292d416f8a94e98ce3f45dd47a9c6683240eb
--get <angiedelgado08@gmail.com>
2019-04-16 08:18:28 +0000

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""python script that takes in a letter and sends a POST with letter as pam"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if (len(argv) < 2):
+        values = {'q': ""}
+    else:
+        values = {'q': argv[1]}
+    r = requests.post('http://0.0.0.0:5000/search_user', values)
+    try:
+        dict_1 = r.json()
+    except:
+        print("Not a valid JSON")
+    if not dict_1:
+        print("No result")
+    else:
+        print("[{}] {}".format(dict_1['id'], dict_1['name']))
