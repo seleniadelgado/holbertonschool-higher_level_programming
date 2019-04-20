0c44325f199506b328934582184dddfa158a6b5a
--get <angiedelgado08@gmail.com>
2019-04-20 02:22:55 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,11 +3,11 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-     username = argv[1]
-     password = argv[2]
-     u = requests.get('https://api.github.com/user', auth=(username,password))
-     if u.status_code == 200:
-         u = u.json()
-         print("{}".format(u['id']))
-     else:
-         print("None")
+    username = argv[1]
+    password = argv[2]
+    u = requests.get('https://api.github.com/user', auth=(username, password))
+    if u.status_code == 200:
+        u = u.json()
+        print("{}".format(u['id']))
+    else:
+        print("None")
