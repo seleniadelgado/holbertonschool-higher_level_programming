3b2cdb36352a505882b32501604e68b3f1c438de
--get <angiedelgado08@gmail.com>
2019-05-13 21:22:56 +0000

    changed object declaration

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      var myObject = {};
+      var myObj = {};
     } else {
       this.width = w;
       this.height = h;
