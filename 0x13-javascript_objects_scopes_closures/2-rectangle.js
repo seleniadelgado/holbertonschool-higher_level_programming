541278a3f0c2b7ddf5f823d1bd311cfbaef46a66
--get <angiedelgado08@gmail.com>
2019-05-13 21:51:49 +0000

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      let myobj = {};
-    } else {
+    if (!isNaN(w) || !isNaN(h) || w >= 0 || h >= 0) {
       this.width = w;
       this.height = h;
     }
   }
-}
-;
+};
