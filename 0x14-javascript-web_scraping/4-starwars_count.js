e7a8dc0b17484c737a10c1b6340096204b624b65
--get <angiedelgado08@gmail.com>
2019-05-15 02:36:40 +0000

    changed if to includes instead of ==

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,17 +10,17 @@ let NumberResults;
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-} else {
-    results = JSON.parse(body).results
-    NumberResults = results.length
-    for (i = 0; i < NumberResults; i++){
-	characters = results[i].characters;
-	for (c = 0; c < characters.length; c++) {
-           if (characters[c] == "https://swapi.co/api/people/18/") {
-		count += 1;
-}
-}
-}
-    console.log(count)
- }
+  } else {
+    results = JSON.parse(body).results;
+    NumberResults = results.length;
+    for (i = 0; i < NumberResults; i++) {
+      characters = results[i].characters;
+      for (c = 0; c < characters.length; c++) {
+        if (characters[c].includes('18')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
 });
