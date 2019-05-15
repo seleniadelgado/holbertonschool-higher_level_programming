eb84e8a97793854866b342e8e7d74df581228e58
--get <angiedelgado08@gmail.com>
2019-05-15 01:06:32 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-//script that matches episode number to a given integer
+// script that matches episode number to a given integer
 // get episode number
 // request API with the correct endpoint
 const request = require('request');
-let url = "http://swapi.co/api/films/"
+let url = 'http://swapi.co/api/films/';
 let episode = process.argv[2];
-let new_path = url + episode
-request (new_path, function (error, response, body) {
+let path = url + episode;
+request(path, function (error, response, body) {
   if (error) {
-   console.log(error);
-} else
-   console.log(JSON.parse(body).title)
+    console.log(error);
+  } else { console.log(JSON.parse(body).title); }
 }
 );
