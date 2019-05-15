3f1a1cd904c2ea63aeec4578b26ac5fd2bd2e940
--get <angiedelgado08@gmail.com>
2019-05-15 02:19:34 +0000

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+let url = process.argv[2];
+let results;
+let count = 0;
+let i;
+let c;
+let characters;
+let NumberResults;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    results = JSON.parse(body).results;
+    NumberResults = results.length;
+    for (i = 0; i < NumberResults; i++) {
+      characters = results[i].characters;
+      for (c = 0; c < characters.length; c++) {
+        if (characters[c] === 'https://swapi.co/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
